2c3a48085058b58feb79145a39988f5aace21269
marioKart <82158691+FreeWisp@users.noreply.github.com>
2025-11-19 22:14:53 +0100

    Delete myLibrary directory

--- myLibrary/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-/* doesn't work with source level 1.8:
-module gestionemagazzino.mylibrary {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens gestionemagazzino.mylibrary to javafx.fxml;
-    exports gestionemagazzino.mylibrary;
-}
-*/
